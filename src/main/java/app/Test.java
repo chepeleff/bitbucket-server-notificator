a86e0188c55093837f395493c5a4b71de8e33af1
chepeleff <a.chepeleff@gmail.com>
2017-08-17 16:56:50 +0300

    current

---
+++ src/main/java/app/Test.java
@@ -0,0 +1,36 @@
+package app;
+
+
+
+/**
+ * Created by ironman on 17.08.2017.
+ */
+public class Test {
+}
+
+
+class Quote {
+    private String type;
+    private String value;
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return "";
+    }
+}
